37113911fee60da05c6c35e46e47b25a4346fb2f
Bence Balint <bence.balint@telenav.com>
2018-04-12 16:09:01 +0300

    new error method

--- js/request2.js
+++ js/request2.js
@@ -1,13 +1,19 @@
+function set_error(message){
+  document.getElementById("number1").style.color = "red";
+  document.getElementById("number2").style.color = "red";
+  alert(message);
+}
+
 function sendRequest() {
   var nr1 = document.getElementById("nr1").value;
   var nr2 = document.getElementById("nr2").value;
 
   if (nr1 == "" || nr2 == "") {
-    alert("No number added!");
+    set_error("No number added!");
   }
 
   if (isNaN(nr1) || isNaN(nr2)) {
-    alert("Only numbers!");
+    set_error("Only numbers!");
   }
 
   var http = new XMLHttpRequest();
